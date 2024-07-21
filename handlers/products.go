3b738cf7b8089f662c5a29659a227f92232f6f84
athrvz <atharvazagade069@gmail.com>
2024-07-21 11:24:20 +0530

    adding PUT api

--- handlers/products.go
+++ handlers/products.go
@@ -4,6 +4,8 @@ import (
 	"go-microserve/product-api/data"
 	"log"
 	"net/http"
+	"regexp"
+	"strconv"
 )
 
 type Products struct {
@@ -16,20 +18,73 @@ func NewProducts(l *log.Logger) *Products {
 
 func (p *Products) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	if r.Method == http.MethodGet {
-		p.GetProducts(rw, r)
+		p.getProducts(rw, r)
 		return
 	}
 
-	// handle update
+	if r.Method == http.MethodPost {
+		p.addProduct(rw, r)
+		return
+	}
 
+	if r.Method == http.MethodPut {
+		regex := regexp.MustCompile(`/([0-9]+)`)
+		g := regex.FindAllStringSubmatch(r.URL.Path, -1)
+		if len(g) != 1 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		if len(g[0]) != 2 {
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+			return
+		}
+
+		idString := g[0][1]
+		id, err := strconv.Atoi(idString)
+		if err != nil {
+			p.l.Println("Invalid URI, unable to convert to number ", idString)
+			http.Error(rw, "Invalid URI", http.StatusBadRequest)
+		}
+
+		p.updateProducts(id, rw, r)
+		return
+	}
 	// catch all
 	rw.WriteHeader(http.StatusMethodNotAllowed)
 }
 
-func (P *Products) GetProducts(rw http.ResponseWriter, h *http.Request) {
+func (p Products) updateProducts(id int, rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle PUT Product")
+	prod := &data.Product{}
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	err = data.UpdateProdut(id, prod)
+	if err == data.ErrProductNotFound {
+		http.Error(rw, "Product not found", http.StatusNotFound)
+		return
+	}
+}
+
+func (P *Products) getProducts(rw http.ResponseWriter, h *http.Request) {
 	lp := data.GetProducts()
 	err := lp.ToJSON(rw)
 	if err != nil {
 		http.Error(rw, "Unable to marshal json", http.StatusInternalServerError)
 	}
+}
+
+func (p *Products) addProduct(rw http.ResponseWriter, r *http.Request) {
+	p.l.Println("Handle POST Product")
+	prod := &data.Product{}
+
+	err := prod.FromJSON(r.Body)
+	if err != nil {
+		http.Error(rw, "Unable to unmarshal json", http.StatusBadRequest)
+	}
+
+	data.AddProduct(prod)
 }
